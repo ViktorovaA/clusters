3bca5fe698eee987448a619be83c9f6e0932219e
Anastasia Viktorova <viktorovaa.04@gmail.com>
2025-01-15 21:48:11 +0300

    add parallel functions

--- src/main/kotlin/com/example/clusterBoundaries/util/boundaring.kt
+++ src/main/kotlin/com/example/clusterBoundaries/util/boundaring.kt
@@ -4,22 +4,36 @@ import com.example.clusterBoundaries.repository.BoundaryRepository
 import com.example.clusterBoundaries.repository.ClusterRepository
 import com.example.clusterBoundaries.repository.PointMetadataRepository
 import com.example.clusterBoundaries.repository.entity.Boundary
-import com.example.clusterBoundaries.repository.entity.Point
+import kotlinx.coroutines.*
+
+//fun createAllClusterBoundaries(
+//        clusterRepository: ClusterRepository,
+//        boundaryRepository: BoundaryRepository,
+//        pointMetadataRepository: PointMetadataRepository,
+//): List<Boundary> {
+//    var i = 1
+//    for (c in clusterRepository.getAll()) {
+//        val curBound = mutableListOf<Point>()
+//        for (p in c.points) {
+//            if (pointMetadataRepository.isBoundary(p.id)) curBound.add(p)
+//        }
+//        boundaryRepository.addItem(Boundary(i, c.id, curBound.toList()))
+//        i++
+//    }
+//    return boundaryRepository.getAll()
+//}
 
 fun createAllClusterBoundaries(
     clusterRepository: ClusterRepository,
     boundaryRepository: BoundaryRepository,
     pointMetadataRepository: PointMetadataRepository,
-): List<Boundary> {
-    var i = 1
-    for (c in clusterRepository.getAll()) {
-        val curBound = mutableListOf<Point>()
-        for (p in c.points) {
-            if (pointMetadataRepository.isBoundary(p.id)) curBound.add(p)
+): List<Boundary> = runBlocking {
+    val boundaries = clusterRepository.getAll().mapIndexed { index, cluster ->
+        async {
+            val curBound = cluster.points.filter { pointMetadataRepository.isBoundary(it.id) }
+            boundaryRepository.addItem(Boundary(index + 1, cluster.id, curBound))
         }
-        boundaryRepository.addItem(Boundary(i, c.id, curBound.toList()))
-        i++
     }
-    return boundaryRepository.getAll()
+    boundaries.awaitAll()
+    return@runBlocking boundaryRepository.getAll()
 }
-
--- src/main/kotlin/com/example/clusterBoundaries/util/clustering.kt
+++ src/main/kotlin/com/example/clusterBoundaries/util/clustering.kt
@@ -4,6 +4,8 @@ import com.example.clusterBoundaries.model.ClusteringSettings
 import com.example.clusterBoundaries.repository.PointMetadataRepository
 import com.example.clusterBoundaries.repository.entity.Cluster
 import com.example.clusterBoundaries.repository.entity.Point
+import kotlinx.coroutines.*
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -19,41 +21,104 @@ fun sg(x: Double): Int {
     return if (x >= 0) 1 else 0
 }
 
+//fun getNearestPoints(
+//        points: List<Point>,
+//        settings: ClusteringSettings,
+//        pointMetadataRepository: PointMetadataRepository,
+//): Map<Point, PointVicinity> {
+//    val pointsVicinity: MutableMap<Point, PointVicinity> = mutableMapOf()
+//    pointMetadataRepository.clear()
+//    for (i in points) {
+//        val pv = PointVicinity()
+//        for (j in points) {
+//            if (i.id == j.id) continue
+//            if (dist(i, j) >= settings.maxDistance) continue
+//            val quadrant = "${sg(j.x - i.x)}${sg(j.y - i.y)}".toInt(2)
+//            pv.quadrants[quadrant].add(j)
+//        }
+//        pointsVicinity[i] = pv
+//        if (pv.quadrants.any { it.isEmpty() }) {
+//            pointMetadataRepository.markAsBoundary(i.id)
+//        }
+//    }
+//    return pointsVicinity
+//}
+//
+//
+//fun addPointRecursively(cluster: MutableSet<Point>, pointsVicinity: MutableMap<Point, PointVicinity>, curPoint: Point) {
+//    cluster.add(curPoint)
+//    val quadrants = pointsVicinity[curPoint]?.quadrants
+//    pointsVicinity.remove(curPoint)
+//
+//    if (quadrants == null) return
+//    for (q in quadrants) {
+//        for (p in q) {
+//            if (cluster.contains(p)) continue
+//
+//            addPointRecursively(cluster, pointsVicinity, p)
+//        }
+//    }
+//}
+//
+//fun clustering(
+//        points: List<Point>,
+//        settings: ClusteringSettings,
+//        pointMetadataRepository: PointMetadataRepository,
+//): List<Cluster> {
+//    val pointsVicinity = getNearestPoints(points, settings, pointMetadataRepository).toMutableMap()
+//
+//    val clusters = mutableListOf<Cluster>()
+//    var i = 1
+//
+//    while (pointsVicinity.isNotEmpty()) {
+//        val curCluster = mutableSetOf<Point>()
+//        addPointRecursively(curCluster, pointsVicinity, pointsVicinity.keys.first())
+//        clusters.add(Cluster(i, curCluster.toList()))
+//        i++
+//    }
+//    return clusters
+//}
+
 fun getNearestPoints(
     points: List<Point>,
     settings: ClusteringSettings,
     pointMetadataRepository: PointMetadataRepository,
-): Map<Point, PointVicinity> {
-    val pointsVicinity: MutableMap<Point, PointVicinity> = mutableMapOf()
+): ConcurrentHashMap<Point, PointVicinity> = runBlocking {
+    val pointsVicinity = ConcurrentHashMap<Point, PointVicinity>()
+
     pointMetadataRepository.clear()
-    for (i in points) {
-        val pv = PointVicinity()
-        for (j in points) {
-            if (i.id == j.id) continue
-            if (dist(i, j) >= settings.maxDistance) continue
-            val quadrant = "${sg(j.x - i.x)}${sg(j.y - i.y)}".toInt(2)
-//            println("$i, $j, $quadrant")
-            pv.quadrants[quadrant].add(j)
+    // Параллельная обработка точек
+    points.map { point ->
+        async(Dispatchers.Default) {
+            val pv = PointVicinity()
+            for (j in points) {
+                if (point.id == j.id) continue
+                if (dist(point, j) >= settings.maxDistance) continue
+                val quadrant = "${sg(j.x - point.x)}${sg(j.y - point.y)}".toInt(2)
+                pv.quadrants[quadrant].add(j)
+            }
+            pointsVicinity[point] = pv
+            if (pv.quadrants.any { it.isEmpty() }) {
+                pointMetadataRepository.markAsBoundary(point.id)
+            }
         }
-        pointsVicinity[i] = pv
-        if (pv.quadrants.any { it.isEmpty() }) {
-            pointMetadataRepository.markAsBoundary(i.id)
-        }
-    }
-    return pointsVicinity
+    }.awaitAll()
+
+    return@runBlocking pointsVicinity
 }
 
-
-fun addPointRecursively(cluster: MutableSet<Point>, pointsVicinity: MutableMap<Point, PointVicinity>, curPoint: Point) {
+fun addPointRecursively(
+    cluster: MutableSet<Point>,
+    pointsVicinity: ConcurrentHashMap<Point, PointVicinity>,
+    curPoint: Point,
+) {
     cluster.add(curPoint)
-    val quadrants = pointsVicinity[curPoint]?.quadrants
-    pointsVicinity.remove(curPoint)
+    val quadrants = pointsVicinity.remove(curPoint)?.quadrants ?: return
 
-    if (quadrants == null) return
-    for (q in quadrants) {
-        for (p in q) {
-            if (cluster.contains(p)) continue
-
+    // Параллельное добавление соседних точек в кластер
+    quadrants.forEach { q ->
+        q.forEach { p ->
+            if (cluster.contains(p)) return@forEach
             addPointRecursively(cluster, pointsVicinity, p)
         }
     }
@@ -63,17 +128,18 @@ fun clustering(
     points: List<Point>,
     settings: ClusteringSettings,
     pointMetadataRepository: PointMetadataRepository,
-): List<Cluster> {
-    val pointsVicinity = getNearestPoints(points, settings, pointMetadataRepository).toMutableMap()
+): List<Cluster> = runBlocking {
+    val pointsVicinity = getNearestPoints(points, settings, pointMetadataRepository)
 
     val clusters = mutableListOf<Cluster>()
     var i = 1
 
     while (pointsVicinity.isNotEmpty()) {
         val curCluster = mutableSetOf<Point>()
-        addPointRecursively(curCluster, pointsVicinity, pointsVicinity.keys.first())
+        val firstPoint = pointsVicinity.keys.first()
+        addPointRecursively(curCluster, pointsVicinity, firstPoint)
         clusters.add(Cluster(i, curCluster.toList()))
         i++
     }
-    return clusters
+    return@runBlocking clusters
 }
